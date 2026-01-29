956169e80a59e082902e45fd20f5a7f54860d672
Aymeric Butet <abutet@f6r3s15.clusters.42paris.fr>
2026-01-29 14:39:01 +0100

    db score request

--- srcs/server/routes/users.routes.ts
+++ srcs/server/routes/users.routes.ts
@@ -101,26 +101,6 @@ export default async function userRoutes(fastify: FastifyInstance) {
     return { ok: true };
   });
 
-  //GESTION DES MATCHS
-
-  fastify.post("/match", async (request) => {
-    const { name_player1, name_player2, score1, score2 } = request.body as any;
-
-    return db.createMatch(name_player1, name_player2, score1, score2);
-  });
-
-  fastify.post("/getMatch", async (request) => {
-    const { name_player } = request.body as any;
-
-    if (!name_player)
-      throw fastify.httpErrors.badRequest("Missing fields");
-
-    if (!db.getUserByUsername(name_player))
-      throw fastify.httpErrors.conflict("User don't exists");
-
-    return db.getMatch(name_player);
-  });
-
   //GESTION DE L'AVATAR
 
   fastify.post("/users/me/setavatar", async (req) => {
